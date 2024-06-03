7d1bf5b5b06c68031330331611e3c30cd8479fcb
OlgaB2022 <babaninaoa@gmail.com>
2024-06-03 20:47:59 +0300

    add 03.06.2024

--- Tested.h
+++ Tested.h
@@ -6,120 +6,120 @@
 #include <string>
 //std::vector<Tested> anyone_rested;
 
-class Tested:public User
+/// <summary>
+/// folder creation —оздание папки
+/// </summary>
+/// <param name="name directory"></param>
+void Add_directory(std::string& name_directory)
+{
+	std::filesystem::path currentPath = std::filesystem::current_path();
+	std::filesystem::path relativePath = currentPath / name_directory;
+
+	std::filesystem::create_directory(relativePath);
+	
+
+	/*name_Person_Data_file = relativePath;
+	name_Person_Data_file = relativePath;
+	name_statictics_file = relativePath;
+	name_login_pass_file = relativePath;
+	name_file_test_incomplete = relativePath;
+	currentPath_ = relativePath;*/
+
+}
+
+
+
+class Tested :public User
 {
 	void Watch_test_work();
-	std::string Encryption(std::string& pass) override
+
+	std::string Encryption(std::string& pass) override;
+	
+	std::filesystem::path name_Person_Data_file;
+	std::filesystem::path name_statictics_file;
+	std::filesystem::path name_login_pass_file;
+	std::filesystem::path name_file_test_incomplete;
+	std::filesystem::path currentPath;
+
+public:
+
+	//Tested();
+
+	void Registration() override
 	{
+		std::cin.get();
+		std::cin.clear();
+
+		std::string name;
+		std::cout << "Enter full name:" << std::endl;
+		std::getline(std::cin, name);
+
+		Add_directory(name);
+
+		std::string login;
+		std::cout << "Enter login:" << std::endl;
+		std::getline(std::cin, login);
+
+		std::string encrypted_login = Encryption(login);
+
+		std::string password;
+		std::cout << "Enter your password:" << std::endl;
+		std::getline(std::cin, password);
+
+		std::string encrypted_password = Encryption(password);
+
+
+		std::ofstream entry_login_pass(name_login_pass_file, std::ios::app);
+		if (!entry_login_pass.is_open())
+		{
+			std::cout << "The file won't open" << std::endl;
+		}
+		else
+		{
+			entry_login_pass << encrypted_login << std::endl;
+			entry_login_pass << encrypted_password << std::endl;
+			entry_login_pass.close();// закрыти€ файла
+		}
+
+
+
+
+
+		std::string addres;
+		std::cout << "Enter home address:" << std::endl;
+		std::getline(std::cin, addres);
+
+
+		std::string phone_number;
+		std::cout << "Enter phone number:" << std::endl;
+		std::getline(std::cin, phone_number);
+
+		std::ofstream entry_info_person(name_Person_Data_file, std::ios::app);
+		if (!entry_info_person.is_open())
+		{
+			std::cout << "The file won't open" << std::endl;
+		}
+		else
+		{
+			entry_info_person << name << std::endl;
+			entry_info_person << addres << std::endl;
+			entry_info_person << phone_number << std::endl;
+
+			entry_info_person.close();// закрыти€ файла
+		}
+
 
 	}
-		std::string name_Person_Data_file;
-		std::string name_statictics_file;
-		std::string name_login_pass_file;
-		std::string name_file_test_incomplete;
-		std::filesystem::path currentPath;
 
-  public:
+	void Work_Test(Test test_);
+	void Watch_Statictics(Test statistics_);
 
-	  Tested();
-	  
-	  void Registration() override
-	  {
-		  std::cin.get();
-		  std::cin.clear();
+	void Check_pass() override;
 
-		  std::string name;
-		  std::cout << "Enter full name:" << std::endl;
-		  std::getline(std::cin, name);
+	void Check_login() override;
+	
 
-		  Add_directory(name);
-		  
-		  std::string login;
-		  std::cout << "Enter login:" << std::endl;
-		  std::getline(std::cin, login);
-
-		  std::string encrypted_login = Encryption(login);
-
-		  std::string password;
-		  std::cout << "Enter your password:" << std::endl;
-		  std::getline(std::cin, password);
-
-		  std::string encrypted_password = Encryption(password);
-
-		 
-		  std::ofstream entry_login_pass(name_login_pass_file, std::ios::app);
-		  if (!entry_login_pass.is_open())
-		  {
-			  std::cout << "The file won't open" << std::endl;
-		  }
-		  else
-		  {
-			  entry_login_pass << encrypted_login << std::endl;
-			  entry_login_pass << encrypted_password << std::endl;
-			  entry_login_pass.close();// закрыти€ файла
-		  }
-
-
-
-		 
-
-		  std::string addres;
-		  std::cout << "Enter home address:" << std::endl;
-		  std::getline(std::cin, addres);
-
-
-		  std::string phone_number;
-		  std::cout << "Enter phone number:" << std::endl;
-		  std::getline(std::cin, phone_number);
-
-		  std::ofstream entry_info_person(name_Person_Data_file, std::ios::app);
-		  if (!entry_info_person.is_open())
-		  {
-			  std::cout << "The file won't open" << std::endl;
-		  }
-		  else
-		  {
-			  entry_info_person << name << std::endl;
-			  entry_info_person << addres << std::endl;
-			  entry_info_person << phone_number << std::endl;
-
-			  entry_info_person.close();// закрыти€ файла
-		  }
-
-
-	  }
-
-	  void Work_Test(Test test_);
-	  void Watch_Statictics(Test statistics_);
-
-	  void Check_pass() override
-	  {
-		  std::ifstream file_login(name_login_pass_file);
-		  if (file_login.is_open()) //открыли файл
-		  {
-			  std::string line;
-			  while (std::getline(file_login >> std::ws, line))
-			  {
-				  std::cout << line;
-			  }
-			  file_login.close();//ќЅя«ј“≈Ћ№Ќќ «ј –џ“№ файл
-		  }
-	  }
-	  void Check_login() override
-	  {
-		  std::ifstream file_login(name_login_pass_file);
-		  if (file_login.is_open()) //открыли файл
-		  {
-			  std::string line;
-			  while (std::getline(file_login >> std::ws, line))
-			  {
-				  std::cout << line;
-			  }
-			  file_login.close();//ќЅя«ј“≈Ћ№Ќќ «ј –џ“№ файл
-		  }
-	  }
-
-	  void Continue_the_test();
+	void Continue_the_test();
 
 };
 
--- User.h
+++ User.h
@@ -5,42 +5,17 @@
 #include <windows.h>
 #include <filesystem>
 
-std::vector<Tested> anyone_rested;
-std::map < std::string, std::vector<Test>> category_and_all_test;
+//std::vector<Tested> anyone_rested;
+//::map < std::string, std::vector<Test>> category_and_all_test;
 
 __interface User
 {
-	virtual void Registration()=0;
-	virtual void Check_pass()=0;
-	virtual void Check_login()=0;
-	virtual std::string Encryption(std::string& pass) =0;
+	virtual void Registration() = 0;
+	virtual void Check_pass() = 0;
+	virtual void Check_login() = 0;
+	virtual std::string Encryption(std::string& pass) = 0;
 
 };
 
-void Add_directory(std::string name_directory)
-{
-    std::filesystem::path currentPath = std::filesystem::current_path();
-    std::filesystem::path relativePath = currentPath / name_directory;
-    
 
 
-
-    if (std::filesystem::create_directory(relativePath))
-    {
-        std::cout << "Directory created successfully!" << std::endl;
-    }
-    else
-    {
-        std::cerr << "Failed to create directory." << std::endl;
-    }
-
-}
-
-void Add_file(std::string name_file,std::string directory)
-{
-    std::filesystem::path currentPath = std::filesystem::current_path();
-   
-
-
-
-}
